fc58d5646913924bbf449f41879c6c68f786113b
saiki <otoiawasehakotiradayo@gmail.com>
2017-11-23 18:23:14 +0900

    ルーティング整理。gorilla使うようにしてみた。

---
+++ src/go/handleFunc.go
@@ -0,0 +1,65 @@
+package imageFileServer
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type HandlingFuncI interface {
+	save(w http.ResponseWriter, r *http.Request)
+	delete(w http.ResponseWriter, r *http.Request)
+	get(w http.ResponseWriter, r *http.Request)
+}
+
+type prodHandlingFunc struct {
+}
+
+type Handlers struct {
+	index templeteHandler
+}
+
+func (h *prodHandlingFunc) save(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *prodHandlingFunc) delete(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *prodHandlingFunc) get(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (t *templeteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//一度だけテンプレートを読み込む
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("views",
+				t.fileName)))
+	})
+
+	e := t.templ.Execute(w, nil)
+
+	if e != nil {
+		fmt.Println("テンプレートの読み込みに失敗しています")
+	}
+}
+
+type templeteHandler struct {
+	once     sync.Once
+	fileName string
+	templ    *template.Template
+}
+
+//NewProdHandler 本番用ハンドラーを作成
+func NewProdHandler() (*HandlingFuncI, *Handlers) {
+	var hf HandlingFuncI
+	hf = new(prodHandlingFunc)
+	hs := Handlers{
+		index: templeteHandler{fileName: "main/index.html"},
+	}
+	return &hf, &hs
+}
---
+++ src/go/routes.go
@@ -0,0 +1,35 @@
+package imageFileServer
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//CreateRoute 渡されたhandlerとfuncについてrouteを設定する
+func CreateRoute(hf *HandlingFuncI, h *Handlers) *mux.Router {
+	r := mux.NewRouter()
+	//cssやjsを読み込めるようにするHandler
+	r.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public/"))))
+
+	setAPIRoute(r, hf)
+	setRouteExistHandler(r, h)
+
+	// 404のときのハンドラ
+	//r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
+	return r
+}
+
+//Handlerが必要ないrouteの設定
+func setAPIRoute(r *mux.Router, hp *HandlingFuncI) {
+	h := *hp
+	r.HandleFunc("/save", h.save)
+	r.HandleFunc("/get", h.get)
+	r.HandleFunc("/delete", h.delete)
+}
+
+//Handlerが必要なrouteの設定
+//templete読み込みなど
+func setRouteExistHandler(r *mux.Router, h *Handlers) {
+	r.Handle("/index", &(h.index))
+}
