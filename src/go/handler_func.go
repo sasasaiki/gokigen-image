52d43ddd8befb0866400745fbd1bff94cd2f0b6d
saiki <otoiawasehakotiradayo@gmail.com>
2017-11-26 13:49:14 +0900

    保存できた

--- src/go/handler_func.go
+++ src/go/handler_func.go
@@ -1,10 +1,12 @@
 package imageFileServer
 
 import (
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/sasasaiki/image-storage"
 )
 
 //HandlerFuncI ハンドリングすべき全てのfuncを持つ
@@ -16,7 +18,15 @@ type HandlerFuncI interface {
 }
 
 func (h *prodHandlerFunc) save(w http.ResponseWriter, r *http.Request) {
-	log.Println("saveにアクセス")
+	file, fileHeader, e := r.FormFile("image")
+	//本当はハンドラーにもたせた方がいい気がする
+	im := imageStorage.DirImgStorage{}
+	e = im.SaveResizedImage(file, fileHeader.Filename, "newFile", "testDir", 400, 0, 90)
+	if e != nil {
+		outputError(&w, e, "fileの保存")
+		return
+	}
+	io.WriteString(w, "保存成功")
 }
 
 func (h *prodHandlerFunc) update(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +40,11 @@ func (h *prodHandlerFunc) delete(w http.ResponseWriter, r *http.Request) {
 func (h *prodHandlerFunc) get(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	log.Println(vars["name"])
+}
+
+func outputError(w *http.ResponseWriter, e error, message string) {
+	io.WriteString(*w, e.Error())
+	log.Println(message, " エラーが発生しました:", e)
 }
 
 type prodHandlerFunc struct {
